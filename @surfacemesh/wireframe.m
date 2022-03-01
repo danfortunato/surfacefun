d797c23c0c430092a83efc77dd88b0e881650fd1
danfortunato <dan.fortunato@gmail.com>
2022-03-01 00:49:06 -0500

    Add surface options to wireframe

--- @surfacemesh/wireframe.m
+++ @surfacemesh/wireframe.m
@@ -1,6 +1,15 @@
 function wireframe(dom, varargin)
 %WIREFRAME   Plot the wireframe of a surface.
 
+parser = inputParser;
+parser.KeepUnmatched = true;
+parser.addParameter('surface', 'auto', @(s) contains(lower(s), {'auto', 'on', 'off'}));
+parse(parser, varargin{:});
+showSurface = parser.Results.surface;
+varargin = namedargs2cell(parser.Unmatched);
+
+defaultStyle = {'Color', 'k', 'LineStyle', '-', 'LineWidth', 1};
+
 holdState = ishold();
 
 x = dom.x;
@@ -16,21 +25,25 @@ z = cat(3, z{:});
 X = [ x(:,1,:) ; nan(1,1,ne) ; x(:,n,:) ; nan(1,1,ne) ; permute(x(1,:,:), [2 1 3]) ; nan(1,1,ne) ; permute(x(n,:,:), [2 1 3]) ; nan(1,1,ne) ];
 Y = [ y(:,1,:) ; nan(1,1,ne) ; y(:,n,:) ; nan(1,1,ne) ; permute(y(1,:,:), [2 1 3]) ; nan(1,1,ne) ; permute(y(n,:,:), [2 1 3]) ; nan(1,1,ne) ];
 Z = [ z(:,1,:) ; nan(1,1,ne) ; z(:,n,:) ; nan(1,1,ne) ; permute(z(1,:,:), [2 1 3]) ; nan(1,1,ne) ; permute(z(n,:,:), [2 1 3]) ; nan(1,1,ne) ];
-plot3(X(:), Y(:), Z(:), 'k-', 'LineWidth', 1, varargin{:})
+plot3(X(:), Y(:), Z(:), defaultStyle{:}, varargin{:})
 
 % If the plot is not being added to another then plot the surface so
 % that the lines are more easily discernable.
-if ( ~holdState )
+if ( (~holdState && strcmpi(showSurface, 'auto')) || strcmpi(showSurface, 'on') )
     % Plot the surface, making it slightly smaller so lines show up
     % more clearly.
     hold on
     for k = 1:length(dom)
         scl = 0.01;
-        surface(x{k} - scl*vn{k}(:,:,1), ...
-                y{k} - scl*vn{k}(:,:,2), ...
-                z{k} - scl*vn{k}(:,:,3), ...
-                0*x{k}, 'FaceColor', 'w', 'EdgeColor', 'None');
+        surface(dom.x{k} - scl*vn{k}(:,:,1), ...
+                dom.y{k} - scl*vn{k}(:,:,2), ...
+                dom.z{k} - scl*vn{k}(:,:,3), ...
+                0*dom.x{k}, 'FaceColor', 'w', 'EdgeColor', 'k', ...
+                'AmbientStrength', 0.6, 'DiffuseStrength', 0.4, 'SpecularStrength', 0.3);
     end
+end
+
+if ( ~holdState )
     axis equal
 end
 
