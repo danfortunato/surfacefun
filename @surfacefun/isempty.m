b7530ef30ba855ff28920a4bacc260e8a0c11040
danfortunato <dan.fortunato@gmail.com>
2022-07-09 16:41:25 -0400

    Fix isempty() for arrays of surfacefuns

--- @surfacefun/isempty.m
+++ @surfacefun/isempty.m
@@ -2,6 +2,6 @@ function out = isempty(f)
 %ISEMPTY   Test for empty SURFACEFUN.
 %   ISEMPTY(F) returns 1 if F is an empty SURFACEFUN and 0 otherwise.
 
-out = isempty(f.vals);
+out = all(size(f) == 1) && isempty(f.vals);
 
 end
