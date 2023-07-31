54f6b241094183241640a042ad81b218ec4357dc
danfortunato <dan.fortunato@gmail.com>
2023-07-31 15:04:41 -0400

    Fix \ and / for quasimatrices

--- @surfacefun/mrdivide.m
+++ @surfacefun/mrdivide.m
@@ -1,11 +1,27 @@
-function f = mrdivide(f, c)
-%/   Right scalar divide for SURFACEFUN.
-%   F/C divides the SURFACEFUN F by a scalar C.
+function X = mrdivide(B, A)
+%\   Right matrix divide for SURFACEFUN objects.
+%   B/A gives the least squares solution to X*A = B, where A, B, and X may
+%   be numeric matrices or SURFACEFUN quasimatrices. For a scalar A and
+%   SURFACEFUN B, B/A divides B by A.
 %
-%   See also RDIVIDE, MLDIVIDE.
+%   See also MLDIVIDE, RDIVIDE.
 
-if ( isnumeric(c) )
-    f = rdivide(f, c);
+if ( isnumeric(A) && isscalar(A) )
+    X = rdivide(B, A);
+elseif ( size(A, 2) ~= size(B, 2) )
+    error('SURFACEFUN:mrdivide:dims', 'Matrix dimensions must agree.')
+elseif ( isnumeric(A) && isa(B, 'surfacefun') )
+    % [INF x M] * [M x N] = [INF x N]:
+    [Q, R] = qr(B);
+    X = Q * (R/A);
+elseif ( isa(A, 'surfacefun') && isnumeric(B) )
+    % [M x INF] * [INF x N] = [M x N]:
+    [Q, R] = qr(A);
+    X = (B/R) * Q';
+elseif ( isa(A, 'surfacefun') && isa(B, 'surfacefun') )
+    % [M x N] * [N x INF] = [M x INF]:
+    [Q, R] = qr(A');
+    X = (B*Q) / R';
 else
     error('SURFACEFUN:mrdivide:mrdivide', 'Not supported. Did you mean ./ ?');
 end
