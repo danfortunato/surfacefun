a092096ec194ea548e9e851c7dc70f63c164c8c8
Dan Fortunato <dan.fortunato@gmail.com>
2025-04-22 13:54:18 -0400

    Speed up surface integrals when all elements are the same order

--- @surfacefun/integral2.m
+++ @surfacefun/integral2.m
@@ -25,12 +25,28 @@ end
 % If a patch uses an N x N discretization, then quadrature is performed on
 % that patch using N points.
 I = zeros(length(f), size(f, 2));
-for k = 1:size(f, 2)
-    for j = 1:length(f(:,k))
-        [nv, nu] = size(f(:,k).vals{j});
-        wu = chebtech2.quadwts(nu); wu = wu(:);
-        wv = chebtech2.quadwts(nv); wv = wv(:);
-        I(j,k) = sum(sum(f(:,k).vals{j} .* wv .* wu.' .* sqrt(f(:,k).domain.J{j})));
+sz1 = cellfun(@(x) size(x,1), f(:,1).vals);
+sz2 = cellfun(@(x) size(x,2), f(:,1).vals);
+if ( all(sz1 == sz1(1)) && all(sz2 == sz2(2)) )
+    % All elements can use the same quadrature weights
+    [nv, nu] = size(f(:,1).vals{1});
+    wu = chebtech2.quadwts(nu); wu = wu(:);
+    wv = chebtech2.quadwts(nv); wv = wv(:);
+    ww = wv .* wu.';
+    for k = 1:size(f, 2)
+        for j = 1:length(f(:,k))
+            I(j,k) = sum(sum(f(:,k).vals{j} .* ww .* sqrt(f(:,k).domain.J{j})));
+        end
+    end
+else
+    % Elements need different quadrature weights
+    for k = 1:size(f, 2)
+        for j = 1:length(f(:,k))
+            [nv, nu] = size(f(:,k).vals{j});
+            wu = chebtech2.quadwts(nu); wu = wu(:);
+            wv = chebtech2.quadwts(nv); wv = wv(:);
+            I(j,k) = sum(sum(f(:,k).vals{j} .* wv .* wu.' .* sqrt(f(:,k).domain.J{j})));
+        end
     end
 end
 
